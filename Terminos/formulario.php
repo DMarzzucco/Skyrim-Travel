b7231f342727ff12e0e6f9c8be18765c5cf7897b
DMarzzucco <123565631+DMarzzucco@users.noreply.github.com>
2023-09-09 19:32:43 -0300

    Add files via upload

---
+++ Terminos/formulario.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>date and time</title>
+    </head>
+    <body>
+        <?php
+        echo('soy un script php');
+        ?>
+    </body>
+</html>
